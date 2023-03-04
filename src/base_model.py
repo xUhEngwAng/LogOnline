f7dba1f1b41c80b320822a5e580aaab459219afd
xUhEngwAng <1551885@tongji.edu.cn>
2023-03-04 17:44:41 +0800

    online update in evaluation phase now uses BCELoss as the loss function.

--- src/base_model.py
+++ src/base_model.py
@@ -7,11 +7,12 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %
 logger = logging.getLogger(__name__)
 
 class BaseModel(torch.nn.Module):
-    def __init__(self, top_k, online_mode=False):
+    def __init__(self, topk, online_mode=False):
         super(BaseModel, self).__init__()
         self.loss = torch.nn.CrossEntropyLoss()
         self.optim = None
-        self.top_k = top_k
+        self.topk = topk
+        self.bottomk = 10
         self.online_mode = online_mode
     
     def evaluate(self, test_loader):
@@ -22,78 +23,106 @@ class BaseModel(torch.nn.Module):
             model = self.eval()
             
         session_dict = {}
+        batch_cnt = 0
+        confident_instances = 0
+        total_loss = 0
         
         for batch in test_loader:
             pred = model.forward(batch)
             
-            # back-propagation in online mode
-            if self.online_mode:
-                label = torch.tensor([next_log['eventid'] for next_log in batch['next']]).to('cuda')
-                batch_loss = self.loss(pred, label)
-                print(f'pred.shape() = {pred.shape}')
-                print(f'label.min() = {label.min()}, label.max() = {label.max()}')
-                
-                self.optim.zero_grad()
-                batch_loss.backward()
-                #total_loss += batch_loss
-                self.optim.step()
+            batch_cnt += 1
+            batch_size = len(batch['next'])
             
-            _, batch_topk_pred = torch.topk(pred, self.top_k)     
+            _, batch_topk_pred = torch.topk(pred, self.topk)
+            _, batch_btmk_pred = torch.topk(pred, self.bottomk, largest=False)
             batch_topk_pred_lst = batch_topk_pred.tolist()
+            batch_btmk_pred_lst = batch_btmk_pred.tolist()
             batch_next_log = [next_log['eventid'] for next_log in batch['next']]
-            matched = []
             
-            for top_k in range(self.top_k):
-                curr_matched = [batch_topk_pred_lst[ind][top_k] == batch_next_log[ind] for ind in range(len(batch_next_log))]
+            matched = []
+            unmatched = [next_log in btmk_pred for next_log, btmk_pred in zip(batch_next_log, batch_btmk_pred_lst)]
+            
+            for topk in range(self.topk):
+                curr_matched = [batch_topk_pred_lst[ind][topk] == batch_next_log[ind] for ind in range(batch_size)]
                 if 0 < len(matched):
                     prev_matched = matched[-1]
                     curr_matched = [prev|curr for prev, curr in zip(prev_matched, curr_matched)]
                 matched.append(curr_matched)
-                            
-            for ind in range(len(batch['session_key'])):
+                
+            # back-propagation in online mode
+            '''
+            if self.online_mode:
+                weight_ = [0 if unmatched[ind] else 1 for ind in range(batch_size)]
+                confident_instances += sum(weight_)
+                weight = torch.tensor(weight_, dtype=torch.float).to('cuda')
+                label = torch.tensor(batch_next_log).to('cuda')
+                loss = torch.nn.CrossEntropyLoss(reduction='none')
+                batch_loss = loss(pred, label)
+                batch_loss = torch.matmul(batch_loss, weight) / (sum(weight_) + 1e-6)
+                
+                self.optim.zero_grad()
+                batch_loss.backward()
+                self.optim.step()
+            '''
+                
+            # Use BCELoss as the loss function
+            if self.online_mode:
+                label = torch.tensor([0 if unmatched[ind] else 1 for ind in range(batch_size)], dtype=torch.float).to('cuda')
+                pred_softmax = torch.nn.functional.softmax(pred)
+                loss = torch.nn.BCELoss()
+                batch_loss = loss(pred_softmax[range(batch_size), batch_next_log], label)
+                total_loss += batch_loss
+                
+                self.optim.zero_grad()
+                batch_loss.backward()
+                self.optim.step()
+
+            for ind in range(batch_size):
                 session_key = batch['session_key'][ind]
                 label = batch['anomaly'][ind]
                 if session_key not in session_dict:
-                    session_dict[session_key] = {f'matched_{top_k}': True for top_k in range(self.top_k)}
+                    session_dict[session_key] = {f'matched_{topk}': True for topk in range(self.topk)}
                     session_dict[session_key]['anomaly'] = False
                 session_dict[session_key]['anomaly'] |= label
                 
-                for top_k in range(self.top_k):
-                    session_dict[session_key][f'matched_{top_k}'] &= matched[top_k][ind]
+                for topk in range(self.topk):
+                    session_dict[session_key][f'matched_{topk}'] &= matched[topk][ind]
+                    
+        # logger.info(f'{confident_instances} instances are used for training in online mode.')
         
-        TP = [0] * self.top_k
-        FP = [0] * self.top_k
+        TP = [0] * self.topk
+        FP = [0] * self.topk
         TON = 0 # total negative
         TOP = 0 # total positive
         
         for key, session_info in session_dict.items():
             if session_info['anomaly']:
                 TOP += 1
-                for top_k in range(self.top_k):
-                    if not session_info[f'matched_{top_k}']:
-                        TP[top_k] += 1
+                for topk in range(self.topk):
+                    if not session_info[f'matched_{topk}']:
+                        TP[topk] += 1
             else:
                 TON += 1
-                for top_k in range(self.top_k):
-                    if not session_info[f'matched_{top_k}']:
-                        FP[top_k] += 1
+                for topk in range(self.topk):
+                    if not session_info[f'matched_{topk}']:
+                        FP[topk] += 1
                     
-        logger.info(f'Evaluation finished. TOP: {TOP}, TON: {TON}.')
-        FN = [TOP - TP[top_k] for top_k in range(self.top_k)]
+        logger.info(f'Evaluation finished. TOP: {TOP}, TON: {TON}, total_loss: {total_loss/batch_cnt :.3f}.')
+        FN = [TOP - TP[topk] for topk in range(self.topk)]
         
-        for top_k in range(self.top_k):
-            if TP[top_k] + FN[top_k] == 0:
+        for topk in range(self.topk):
+            if TP[topk] + FN[topk] == 0:
                 precision = np.NAN
             else:
-                precision = TP[top_k] / (TP[top_k] + FP[top_k])
+                precision = TP[topk] / (TP[topk] + FP[topk])
 
             if TOP == 0:
                 recall = np.NAN
             else:
-                recall = TP[top_k] / TOP
+                recall = TP[topk] / TOP
 
             F1 = 2 * precision * recall / (precision + recall)
-            logger.info(f'[topk={top_k+1}] FP: {FP[top_k]}, FN: {FN[top_k]}, Precision: {precision: .3f}, Recall: {recall :.3f}, F1-measure: {F1: .3f}.')
+            logger.info(f'[topk={topk+1}] FP: {FP[topk]}, FN: {FN[topk]}, Precision: {precision: .3f}, Recall: {recall :.3f}, F1-measure: {F1: .3f}.')
     
     def fit(self, train_loader):
         batch_cnt = 0
